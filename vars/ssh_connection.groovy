d1d476cd5f629cffb403c50f815e316293d71d65
Tenzin Thinlay <bhutenthinlay@gmail.com>
2020-11-02 12:14:33 -0500

    Update ssh_connection.groovy

--- vars/ssh_connection.groovy
+++ vars/ssh_connection.groovy
@@ -8,10 +8,10 @@ def call () {
 	stage('Remote SSH') {
 		//sshCommand remote: remote, command: "uname -a"
 		//sshCommand remote: remote, command: "use -i libc.so"
-		//sshCommand remote: remote, command: "export PATH = /proc/boot:$PATH"
-		//sshCommand remote: remote, command: "echo $PATH"
-		//sshCommand remote: remote, command: "ls", sudo: true
-		sh "ls"
+		sshCommand remote: remote, command: "export PATH=$PATH:/proc/boot"
+		sshCommand remote: remote, command: "echo $PATH"
+		sshCommand remote: remote, command: "ls", sudo: true
+		//sh "ls"
 		//def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh"
 		//if(commandResult.contain("Password:")){
 		//	sshCommand remote: remote, command: "root\n"
