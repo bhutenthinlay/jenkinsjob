5d733f80c70a2c3190b19bf2898abd1839e88ae5
Tenzin Thinlay <bhutenthinlay@gmail.com>
2020-11-04 10:49:32 -0500

    Update ssh_connection.groovy

--- vars/ssh_connection.groovy
+++ vars/ssh_connection.groovy
@@ -11,12 +11,12 @@ def call () {
 		sshCommand remote: remote, command: "echo $PATH"
 		//writeFile file: 'abc.sh', text: 'ls'
 		//sshCommand remote: remote, command: "cd /proc/boot"
-		sshCommand remote: remote, command: "env"
+		sshCommand remote: remote, command: "export PATH=/proc/boot:/bin:/usr/bin:/sbin:/usr/sbin:/home/vsx/vsx0/../bin:/home/vsx/vsx0/BIN"
     		//sshScript remote: remote, script: "abc.sh"
 		//sshCommand remote: remote, command: "use -i libc.so"
 		//sshCommand remote: remote, command: "export PATH="
-		//sshCommand remote: remote, command: "echo $PATH"
-		//sshCommand remote: remote, command: "./ls"
+		sshCommand remote: remote, command: "echo $PATH"
+		sshCommand remote: remote, command: "ls"
 		//sh "echo $PATH"
 		//def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh"
 		//if(commandResult.contain("Password:")){
