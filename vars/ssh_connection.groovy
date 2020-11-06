f1582932546000de378567b80cfb2b10b8e025bf
Tenzin Thinlay <bhutenthinlay@gmail.com>
2020-11-06 13:44:27 -0500

    Update ssh_connection.groovy

--- vars/ssh_connection.groovy
+++ vars/ssh_connection.groovy
@@ -8,7 +8,9 @@ def call () {
 	remote.allowAnyHosts = true
 	remote.pty = true
 	stage('Remote SSH') {
-		def commandResult = sshCommand remote: remote, command: "echo 'root' | sh VSX_QNX/pse54_setup.sh"
+		//def commandResult = sshCommand remote: remote, command: "echo 'root' | sh VSX_QNX/pse54_setup.sh"
+		def commandResult = sshCommand remote: remote, command: "pwd"
+		println(commandResult)
 		//if(commandResult.indexOf("password")){
 		//	sshCommand remote: remote, command: "root\n"
 		//}
