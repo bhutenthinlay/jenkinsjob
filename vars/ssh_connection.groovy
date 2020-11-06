b4d496c4b82650e2dd6610b7d8df37e5ff6f4e9a
Tenzin Thinlay <bhutenthinlay@gmail.com>
2020-11-06 10:26:25 -0500

    Update ssh_connection.groovy

--- vars/ssh_connection.groovy
+++ vars/ssh_connection.groovy
@@ -8,21 +8,9 @@ def call () {
 	remote.allowAnyHosts = true
 	remote.pty = true
 	stage('Remote SSH') {
-		//writeFile file: 'abc.sh', text: 'export PATH=/proc/boot:/bin:/usr/bin:/sbin:/usr/sbin:/home/vsx/vsx0/../bin:/home/vsx/vsx0/BIN echo $PATH'
-    		//sshScript remote: remote, script: "abc.sh"
-		//sshCommand remote: remote, command: "echo $PATH"
-		//writeFile file: 'abc.sh', text: 'ls'
-		//sshCommand remote: remote, command: "env"
-		//sshCommand remote: remote, command: "PATH=/proc/boot:/bin:/usr/bin:/sbin:/usr/sbin:/home/vsx/vsx0/../bin:/home/vsx/vsx0/BIN TET_ROOT=/home/vsx USER=vsx0 LD_LIBRARY_PATH=/proc/boot:/lib:/usr/lib:/lib/dll POSIXLY_CORRECT=1 HOME=/home/vsx/vsx0 QNX_HOST=/opt/qnx/sdp/x86_64 QNX_TARGET=/opt/qnx/sdp TET_SUITE_ROOT=/home/vsx TERM=xterm TZ=EST5EDT LOCFILE=/usr/share/locale/locale.file TET_EXECUTE=/home/vsx/vsx0/TESTROOT LOCALE=C-TRADITIONAL SSH_TTY=/dev/ttyp1 ENV=.kshrc LOGNAME=vsx0 ls env"
-    		//sshScript remote: remote, script: "abc.sh"
-		//sshCommand remote: remote, command: "use -i libc.so"
-		//sshCommand remote: remote, command: "export PATH="
-		//sshCommand remote: remote, command: "echo $PATH"
-		//sshCommand remote: remote, command: "ls"
-		//sh "echo $PATH"
 		def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh"
-		//if(commandResult.contain("Enter the root password to proceed")){
-		//	sshCommand remote: remote, command: "root\n"
-		//}
+		if(commandResult.contains("Enter the root password to proceed")){
+			sshCommand remote: remote, command: "root\n"
+		}
 	}
 }
