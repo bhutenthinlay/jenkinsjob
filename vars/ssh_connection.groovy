40b9083ca2da40ab516fec7d45068f83a9712231
Tenzin Thinlay <bhutenthinlay@gmail.com>
2020-11-09 18:40:44 -0500

    Update ssh_connection.groovy

--- vars/ssh_connection.groovy
+++ vars/ssh_connection.groovy
@@ -12,13 +12,13 @@ def call (user) {
 	}else{
 	 println(" in vsx0")
 	}
-	stage('Remote SSH') {
-		def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh; root\n"
+	//stage('Remote SSH') {
+	//	def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh; root\n"
 		//def commandResult = sshCommand remote: remote, command: "pwd"
-		println(commandResult)
-		if(commandResult.indexOf("password")){
+	//	println(commandResult)
+	//	if(commandResult.indexOf("password")){
 			//sshCommand remote: remote, command: "root\n"
-			println("yes it works")
-		}
-	}
+	//		println("yes it works")
+	//	}
+	//}
 }
