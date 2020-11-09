435625e720117d3e1e81d6bceb77f4a8f135d651
Tenzin Thinlay <bhutenthinlay@gmail.com>
2020-11-09 18:39:58 -0500

    Update ssh_connection.groovy

--- vars/ssh_connection.groovy
+++ vars/ssh_connection.groovy
@@ -1,12 +1,17 @@
 #!/usr/bin/groovy
-def call () {
+def call (user) {
 	def remote = [:]
 	remote.name = 'pse54'
 	remote.host = '172.16.201.131'
-	remote.user = 'vsx0'
-	remote.password = 'vsx0'
+	remote.user = user
+	remote.password = user
 	remote.allowAnyHosts = true
 	remote.pty = true
+	if(user == "root"){
+	 println("in root")
+	}else{
+	 println(" in vsx0")
+	}
 	stage('Remote SSH') {
 		def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh; root\n"
 		//def commandResult = sshCommand remote: remote, command: "pwd"
