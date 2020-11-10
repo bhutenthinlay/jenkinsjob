3348d0c545d1abfe3817fa48e8881032ecbbbfac
Tenzin Thinlay <bhutenthinlay@gmail.com>
2020-11-09 19:39:28 -0500

    Update ssh_connection.groovy

--- vars/ssh_connection.groovy
+++ vars/ssh_connection.groovy
@@ -16,10 +16,10 @@ def call (user) {
 	 println(" in vsx0")
 		stage('Remote SSH') {
 			//sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh"
-			sshCommand remote: remote, command: "tcc -b -s pse54posix.bld;"
-			sshCommand remote: remote, command: "tcc -b -s pse54xsi.bld"
-			sshCommand remote: remote, command: "tcc -e -s pse54posix.exec"
-			sshCommand remote: remote, command: "tcc -e -s pse54xsi.exec"
+			sshCommand remote: remote, command: "/home/vsx/vsx0/../bin/tcc -b -s pse54posix.bld"
+			sshCommand remote: remote, command: "/home/vsx/vsx0/../bin/tcc -b -s pse54xsi.bld"
+			sshCommand remote: remote, command: "/home/vsx/vsx0/../bin/tcc -e -s pse54posix.exec"
+			sshCommand remote: remote, command: "/home/vsx/vsx0/../bin/tcc -e -s pse54xsi.exec"
 		}
 	}
 	//stage('Remote SSH') {
