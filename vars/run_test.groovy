a10737281c3630686c20cea4e351b2c140b2a11e
Tenzin Thinlay <bhutenthinlay@gmail.com>
2020-11-09 20:06:51 -0500

    Update run_test.groovy

--- vars/run_test.groovy
+++ vars/run_test.groovy
@@ -3,8 +3,8 @@ def call () {
 	def remote = [:]
 	remote.name = 'pse54'
 	remote.host = '172.16.201.131'
-	remote.user = vsx0
-	remote.password = vsx0
+	remote.user = 'vsx0'
+	remote.password = 'vsx0'
 	remote.allowAnyHosts = true
 	remote.pty = true
 	 	stage('Remote SSH') {
