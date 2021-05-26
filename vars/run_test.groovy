9725a234d0dc92d36a2b55247bf5b3a1aeb10e31
Tenzin Thinlay <bhutenthinlay@gmail.com>
2021-05-26 12:41:28 -0400

    Update run_test.groovy

--- vars/run_test.groovy
+++ vars/run_test.groovy
@@ -15,10 +15,10 @@ def call () {
 		println("Running commands")
 		def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh"
 // 		def commandResult = sshCommand remote: remote, command: "pwd"
-		println(commandResult)
-		if(commandResult.indexOf("password")){
-			sshCommand remote: remote, command: "root\n"
-			println("yes it works")
+// 		println(commandResult)
+// 		if(commandResult.indexOf("password")){
+// 			sshCommand remote: remote, command: "root\n"
+// 			println("yes it works")
 		}
 	}
 }
