8d61a2306ff4499125b067fd95a0a2dea70fb29e
Tenzin Thinlay <bhutenthinlay@gmail.com>
2021-05-26 10:51:05 -0400

    Update run_test.groovy

--- vars/run_test.groovy
+++ vars/run_test.groovy
@@ -7,17 +7,17 @@ def call () {
 	remote.password = 'vsx0'
 	remote.allowAnyHosts = true
 	remote.pty = true
-	 	stage('Remote SSH') {
-			sshCommand remote: remote, command: "sh /home/vsx/vsx0/VSX_QNX/runtest.sh"
-		}
+// 	 	stage('Remote SSH') {
+// 			sshCommand remote: remote, command: "sh /home/vsx/vsx0/VSX_QNX/runtest.sh"
+// 		}
 	
-	//stage('Remote SSH') {
-	//	def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh; root\n"
-		//def commandResult = sshCommand remote: remote, command: "pwd"
-	//	println(commandResult)
-	//	if(commandResult.indexOf("password")){
-			//sshCommand remote: remote, command: "root\n"
-	//		println("yes it works")
-	//	}
-	//}
+	stage('Remote SSH') {
+		def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh; root\n"
+		def commandResult = sshCommand remote: remote, command: "pwd"
+		println(commandResult)
+		if(commandResult.indexOf("password")){
+			sshCommand remote: remote, command: "root\n"
+			println("yes it works")
+		}
+	}
 }
