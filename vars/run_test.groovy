ed4dab3ea92d4f04b69fbafb6915a2b7aa8775de
Tenzin Thinlay <bhutenthinlay@gmail.com>
2021-05-26 10:55:04 -0400

    Update run_test.groovy

--- vars/run_test.groovy
+++ vars/run_test.groovy
@@ -2,7 +2,7 @@
 def call () {
 	def remote = [:]
 	remote.name = 'pse54'
-	remote.host = '172.16.201.131'
+	remote.host = '172.16.201.134'
 	remote.user = 'vsx0'
 	remote.password = 'vsx0'
 	remote.allowAnyHosts = true
