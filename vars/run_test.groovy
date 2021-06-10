f55c68e632d4c2bf269e878a71cc9f1a5f504f50
Tenzin Thinlay <bhutenthinlay@gmail.com>
2021-06-10 11:31:34 -0400

    Update run_test.groovy

--- vars/run_test.groovy
+++ vars/run_test.groovy
@@ -1,25 +1,32 @@
-#!/usr/bin/groovy
-def call () {
-	def remote = [:]
-	remote.name = 'pse54'
-	remote.host = '172.16.201.134'
-	remote.user = 'vsx0'
-	remote.password = 'vsx0'
- 	remote.allowAnyHosts = true
-// 	remote.logLevel = 'FINEST'
-// 	remote.pty = true
-// 	 	stage('Remote SSH') {
-// 			sshCommand remote: remote, command: "sh /home/vsx/vsx0/VSX_QNX/runtest.sh"
-// 		}
+// #!/usr/bin/groovy
+// def call () {
+// 	def remote = [:]
+// 	remote.name = 'pse54'
+// 	remote.host = '172.16.201.134'
+// 	remote.user = 'vsx0'
+// 	remote.password = 'vsx0'
+//  	remote.allowAnyHosts = true
+// // 	remote.logLevel = 'FINEST'
+// // 	remote.pty = true
+// // 	 	stage('Remote SSH') {
+// // 			sshCommand remote: remote, command: "sh /home/vsx/vsx0/VSX_QNX/runtest.sh"
+// // 		}
 	
-	stage('Remote SSH') {
-		println("Running commands")
-		def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh"
-// 		def commandResult = sshCommand remote: remote, command: "pwd"
-// 		println(commandResult)
-// 		if(commandResult.indexOf("password")){
-// 			sshCommand remote: remote, command: "root\n"
-// 			println("yes it works")
-// 		}
-	}
+// 	stage('Remote SSH') {
+// 		println("Running commands")
+// 		def commandResult = sshCommand remote: remote, command: "sh VSX_QNX/pse54_setup.sh"
+// // 		def commandResult = sshCommand remote: remote, command: "pwd"
+// // 		println(commandResult)
+// // 		if(commandResult.indexOf("password")){
+// // 			sshCommand remote: remote, command: "root\n"
+// // 			println("yes it works")
+// // 		}
+// 	}
+// }
+def call(){
+	sh """
+	#!/bin/bash
+	pwd
+	exit 0
+	"""
 }
